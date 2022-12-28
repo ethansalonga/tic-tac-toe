32ee83ba4fcf0416a08a30c174ffb6e67b889c11
Ethan Salonga <ethansalonga28@gmail.com>
2022-12-28 17:11:02 -0500

    Update gameplay

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-const boardTitle = document.querySelector(".board__title")
-const boardSquares = document.querySelectorAll(".board__square")
+const allSquares = document.querySelectorAll(".board__square")
+const title  = document.querySelector(".board__title")
 
 let currentPlayer = "X"
 
-function handleTurn(square) {
-  square.disabled = true
+allSquares.forEach((square, i) => {
+  square.addEventListener("click", () => {
+    console.log("clicked")
+    square.innerHTML = currentPlayer
+    title.innerHTML = `${currentPlayer}'s Turn`
+    currentPlayer = currentPlayer === "X" ? "O" : "X"
+  })
+})
 
-  if (currentPlayer === "X") {
-    square.innerHTML = "X"
-    boardTitle.innerHTML = "O's Turn"
-    currentPlayer = "O"
-  } else {
-    square.innerHTML = "O"
-    boardTitle.innerHTML = "X's Turn"
-    currentPlayer = "X"
-  }
-}
+// const boardTitle = document.querySelector(".board__title")
+// const boardSquares = document.querySelectorAll(".board__square")
 
-function handleReset() {
-  boardSquares.forEach(elem => elem.disabled = false)
-  boardSquares.forEach(elem => elem.innerHTML = "")
+// let currentPlayer = "X"
+
+// function handleTurn(square) {
+//   square.disabled = true
+
+//   if (currentPlayer === "X") {
+//     square.innerHTML = "X"
+//     boardTitle.innerHTML = "O's Turn"
+//     currentPlayer = "O"
+//   } else {
+//     square.innerHTML = "O"
+//     boardTitle.innerHTML = "X's Turn"
+//     currentPlayer = "X"
+//   }
+// }
+
+// function handleReset() {
+//   boardSquares.forEach(elem => elem.disabled = false)
+//   boardSquares.forEach(elem => elem.innerHTML = "")
   
-  boardTitle.innerHTML = "X's Turn"
-  currentPlayer = "X"
-}
+//   boardTitle.innerHTML = "X's Turn"
+//   currentPlayer = "X"
+// }
