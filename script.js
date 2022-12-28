0447ebd335b92bc00d78414cbbed6423795b3945
Ethan Salonga <ethansalonga28@gmail.com>
2022-12-28 17:26:16 -0500

    Set up checkWin function

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
 const allSquares = document.querySelectorAll(".board__square")
-const title  = document.querySelector(".board__title")
+const title = document.querySelector(".board__title")
 
 let currentPlayer = "X"
 
+let board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+
 allSquares.forEach((square, i) => {
   square.addEventListener("click", () => {
-    console.log("clicked")
     square.innerHTML = currentPlayer
+
+    checkWin()
+
     title.innerHTML = `${currentPlayer}'s Turn`
     currentPlayer = currentPlayer === "X" ? "O" : "X"
   })
 })
 
-// const boardTitle = document.querySelector(".board__title")
-// const boardSquares = document.querySelectorAll(".board__square")
+function checkWin() {
+  const winningIndices = [
+    // Horizontal Wins
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical Wins
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal Wins
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
 
-// let currentPlayer = "X"
+  for (let i = 0; i < winningIndices.length; ++i) {
+    const matchingIndices = winningIndices[i]
+    console.log(matchingIndices)
+    let symbol1 = board[matchingIndices[0]]
+    let symbol2 = board[matchingIndices[1]]
+    let symbol3 = board[matchingIndices[2]]
 
-// function handleTurn(square) {
-//   square.disabled = true
-
-//   if (currentPlayer === "X") {
-//     square.innerHTML = "X"
-//     boardTitle.innerHTML = "O's Turn"
-//     currentPlayer = "O"
-//   } else {
-//     square.innerHTML = "O"
-//     boardTitle.innerHTML = "X's Turn"
-//     currentPlayer = "X"
-//   }
-// }
-
-// function handleReset() {
-//   boardSquares.forEach(elem => elem.disabled = false)
-//   boardSquares.forEach(elem => elem.innerHTML = "")
-  
-//   boardTitle.innerHTML = "X's Turn"
-//   currentPlayer = "X"
-// }
+    if (symbol1 === symbol2 && symbol2 === symbol3) {
+      console.log("winner at", matchingIndices);
+    }
+  }
+}
