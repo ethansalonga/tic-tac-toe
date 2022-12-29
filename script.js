c3827ce4440d45185e11aaf3c5e60932ad136557
Ethan Salonga <ethansalonga28@gmail.com>
2022-12-29 14:36:40 -0500

    Reset function

--- script.js
+++ script.js
@@ -30,6 +30,15 @@ allSquares.forEach((square, i) => {
   })
 })
 
+function restartGame() {
+  gameOver = false
+  title.innerHTML = `${currentPlayer}'s Turn'`
+  allSquares.forEach(square => {
+    square.innerHTML = ""
+  })
+  board = newArray(9)
+}
+
 function checkDraw() {
   for (let i = 0; i < board.length; ++i) {
     if (!board[i]) {
